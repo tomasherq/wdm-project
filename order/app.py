633b922a9a73b92c7e89a96718b6d8706977fca2
tomasherq <T.HernandezQuintanilla@student.tudelft.nl>
2022-05-10 19:27:10 +0200

    Example stuff

--- order/app.py
+++ order/app.py
@@ -1,22 +1,26 @@
-import os
-import atexit
+
 
 from flask import Flask
-from common.database import DatabaseMongo
+import sys
+import os
 
 
-gateway_url = os.environ['GATEWAY_URL']
+sys.path.insert(1, os.getcwd())
+if True:
+    from common.tools import *
+
 
 app = Flask("order-service")
 
-collection = DatabaseMongo("orders", "order")
+collection = getCollection("orders", "order")
 
 
 @app.post('/create/<user_id>')
 def create_order(user_id):
-    order_id = collection.find()
 
-    collection.insert([{"name": user_id, "address": "Highway 37"}])
+    order_id = len(list(collection.find({})))
+    collection.insert_one({"order_id": order_id, "name": user_id, "address": "Highway 37"})
+    return response(200, f"Correctly added, orderid {order_id}")
 
 
 @app.delete('/remove/<order_id>')
@@ -36,9 +40,13 @@ def remove_item(order_id, item_id):
 
 @app.get('/find/<order_id>')
 def find_order(order_id):
-    collection.find()
+    order_id = int(order_id)
+    return response(200, collection.find_one({"order_id": order_id}, {"_id": 0}))
 
 
-@app.post('/checkout/<order_id>')
+@ app.post('/checkout/<order_id>')
 def checkout(order_id):
     pass
+
+
+app.run(port=2801)
